932e79f1903f78dcf0984726eb7eaaccac7cebdd
Jack T <jackmt9@gmail.com>
2020-02-26 19:20:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,23 @@
-# Add your code here
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(song_name)
+  
+end
+
+def list
+
+end 
+
+def exit_jukebox
+  
+end
+
+def run
+  
+end
