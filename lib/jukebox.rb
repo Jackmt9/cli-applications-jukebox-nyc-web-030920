61925baed666b13b0291e4343ee0b31875dc61f2
Jack T <jackmt9@gmail.com>
2020-02-26 19:52:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   selection = gets.strip
-  if songs.included?(selection)
+  if songs.include?(selection)
     puts "Playing #{selection}"
   elsif selection.to_i == songs.length + 1
     puts "Playing #{songs[selection.to_i]}"
